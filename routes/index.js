b76d27027f1b00ce6c6a789cd941592e13fcdbe2
Tom Klun <tom@tomklun.com>
2012-09-27 16:53:10 -0500

    Removing values and logic from front end. View can now be run independent of the application.

--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,15 @@
 /*
  * GET home page.
  */
-var QRCode = require('qrcode'),
-    cuid = require('cuid');
-
-var generateGuid = function() {
-  return cuid();
-};
 
 exports.index = function(req, res){
-  var protocol = 'http://',
-      hostname = req.headers.host;
-      path = 'client',
-      guid = generateGuid(),
-      qrCodePath = protocol + hostname + '/' + path + '?guid=' + guid;
-  
-  console.log('Generated path: ', qrCodePath);
-  QRCode.toDataURL(qrCodePath, function(err,url) {
-    res.render('index', {
-      title: 'Node Color Picker',
-      qrcode: url,
-      guid: guid
-    });
+  res.render('index', {
+    title: 'Node Color Picker'
   });
 };
 
 exports.client = function(req, res){
-  console.log('GUID: ', req.query.guid);
   res.render('client', {
-    title: 'Node Color Picker - Client',
-    guid: req.query.guid
+    title: 'Node Color Picker - Client'
   });
 };
